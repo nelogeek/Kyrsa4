076051292d878fa6d60158f9170d11e66a6ba8e5
nelogeek <lokotaevpr@gmail.com>
2021-05-28 00:29:57 +0500

    Initial commit

---
+++ Kyrsa4/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace Kyrsa4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(356, 288);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(64, 22);
+            this.toolStripLabel1.Text = "Выберите:";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 25);
+            this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripComboBox1});
+            this.toolStrip1.Location = new System.Drawing.Point(106, 27);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(199, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Poor Richard", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(446, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 38);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(441, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "№ автобуса";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(413, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Уехал";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(506, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 32);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Приехал";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(388, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(232, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "---------------------------------------------------------------------------";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(385, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(232, 33);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Добавить работника";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(446, 210);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(114, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(389, 258);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(211, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(446, 300);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(114, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(455, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 22);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "№ автобуса";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(444, 233);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 22);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Фамилия И.О.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(456, 279);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 18);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "№ маршрута";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(459, 326);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 36);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Утвердить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 364);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "Form1";
+            this.Text = "Labash";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Kyrsa4/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using SD = System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ExcelDataReader;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Diagnostics;
+using MetroFramework.Components;
+using MetroFramework.Forms;
+
+namespace Kyrsa4
+{
+    public partial class Form1 : MetroForm
+    {
+        string fullPath = Application.StartupPath.ToString() + "\\1.xlsx";
+        private SD.DataTableCollection TableCollection = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            OpenExcelFile(fullPath);
+        }
+
+        private void OpenExcelFile(string path)
+        {
+            FileStream stream = File.Open(fullPath, FileMode.Open, FileAccess.Read);
+            IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream);
+            SD.DataSet db = reader.AsDataSet(new ExcelDataSetConfiguration()
+            {
+                ConfigureDataTable = (x) => new ExcelDataTableConfiguration()
+                {
+                    UseHeaderRow = true
+                }
+            });
+
+            TableCollection = db.Tables;
+            toolStripComboBox1.Items.Clear();
+            foreach (SD.DataTable table in TableCollection)
+            {
+                toolStripComboBox1.Items.Add(table.TableName);
+            }
+
+            toolStripComboBox1.SelectedIndex = 0;
+            stream.Close();
+            reader.Close();
+        }
+
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SD.DataTable table = TableCollection[Convert.ToString(toolStripComboBox1.SelectedItem)];
+            dataGridView1.DataSource = table;
+        }
+
+        
+
+        string val1, val2, val3;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fileName = fullPath; //имя Excel файла  
+            Excel.Application xlApp = new Excel.Application();
+
+            xlApp.ScreenUpdating = false;// !!!ускорение кода!!!
+
+            Excel.Workbook xlWb = xlApp.Workbooks.Open(fileName); //открываем Excel файл
+            Excel.Worksheet xlSht = xlWb.Sheets[1]; //первый лист в файле
+            int iLastRow = xlSht.Cells[xlSht.Rows.Count, "A"].End[Excel.XlDirection.xlUp].Row;  //последняя заполненная строка в столбце А
+            
+            //---------------------------------------
+            int num = Convert.ToInt32(textBox1.Text);
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value) == num){
+                     val1 = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                     val2 = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                     val3 = dataGridView1.Rows[i].Cells[2].Value.ToString();
+                     xlSht.Rows[i+2].Delete();
+
+                    MessageBox.Show(val2.ToString() + " уехал");
+
+                    
+                }
+                
+
+            }
+            xlSht = xlWb.Sheets[2]; //первый лист в файле
+            iLastRow = xlSht.Cells[xlSht.Rows.Count, "A"].End[Excel.XlDirection.xlUp].Row;  //последняя заполненная строка
+            iLastRow++;
+            xlSht.Cells[iLastRow, "A"].Value = val1; //запись в ячейку
+            xlSht.Cells[iLastRow, "B"].Value = val2;
+            xlSht.Cells[iLastRow, "C"].Value = val3;
+
+            xlApp.ScreenUpdating = true;// !!!ускорение кода!!!
+
+            //xlApp.Visible = true;
+            xlWb.Close(true); //закрыть и сохранить книгу
+            xlApp.Quit();
+            //MessageBox.Show("Файл успешно сохранён!");
+            OpenExcelFile(fullPath);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string fileName = fullPath; //имя Excel файла  
+            Excel.Application xlApp = new Excel.Application();
+
+            xlApp.ScreenUpdating = false;// !!!ускорение кода!!!
+
+            Excel.Workbook xlWb = xlApp.Workbooks.Open(fileName); //открываем Excel файл
+            Excel.Worksheet xlSht = xlWb.Sheets[2]; //первый лист в файле
+            int iLastRow = xlSht.Cells[xlSht.Rows.Count, "A"].End[Excel.XlDirection.xlUp].Row;  //последняя заполненная строка в столбце А
+
+            //---------------------------------------
+            int num = Convert.ToInt32(textBox1.Text);
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value) == num)
+                {
+                    val1 = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                    val2 = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                    val3 = dataGridView1.Rows[i].Cells[2].Value.ToString();
+                    xlSht.Rows[i + 2].Delete();
+
+                    MessageBox.Show(val2.ToString() + " приехал");
+
+
+                }
+
+
+            }
+            xlSht = xlWb.Sheets[1]; //первый лист в файле
+            iLastRow = xlSht.Cells[xlSht.Rows.Count, "A"].End[Excel.XlDirection.xlUp].Row;  //последняя заполненная строка
+            iLastRow++;
+            xlSht.Cells[iLastRow, "A"].Value = val1; //запись в ячейку
+            xlSht.Cells[iLastRow, "B"].Value = val2;
+            xlSht.Cells[iLastRow, "C"].Value = val3;
+
+            xlApp.ScreenUpdating = true;// !!!ускорение кода!!!
+
+            //xlApp.Visible = true;
+            xlWb.Close(true); //закрыть и сохранить книгу
+            xlApp.Quit();
+            //MessageBox.Show("Файл успешно сохранён!");
+            OpenExcelFile(fullPath);
+        }
+
+        
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            string fileName = fullPath; //имя Excel файла  
+            Excel.Application xlApp = new Excel.Application();
+
+            xlApp.ScreenUpdating = false;// !!!ускорение кода!!!
+
+            Excel.Workbook xlWb = xlApp.Workbooks.Open(fileName); //открываем Excel файл
+            Excel.Worksheet xlSht = xlWb.Sheets[1]; //первый лист в файле
+            int iLastRow = xlSht.Cells[xlSht.Rows.Count, "A"].End[Excel.XlDirection.xlUp].Row;  //последняя заполненная строка в столбце А
+
+            iLastRow++;
+            xlSht.Cells[iLastRow, "A"].Value = textBox2.Text.ToString(); //запись в ячейку
+            xlSht.Cells[iLastRow, "B"].Value = textBox3.Text.ToString();
+            xlSht.Cells[iLastRow, "C"].Value = textBox4.Text.ToString();
+
+            xlApp.ScreenUpdating = true;// !!!ускорение кода!!!
+
+            //xlApp.Visible = true;
+            xlWb.Close(true); //закрыть и сохранить книгу
+            xlApp.Quit();
+            //MessageBox.Show("Файл успешно сохранён!");
+            OpenExcelFile(fullPath);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string fileName = fullPath; //имя Excel файла  
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWb = xlApp.Workbooks.Open(fileName); //открываем Excel файл
+            Excel.Worksheet xlSht = xlWb.Sheets[1]; //первый лист в файле
+            int iLastRow = xlSht.Cells[xlSht.Rows.Count, "A"].End[Excel.XlDirection.xlUp].Row;  //последняя заполненная строка в столбце А
+            
+            xlSht.Cells[++iLastRow, "A"].Value = Convert.ToString(textBox1.Text);
+            
+            //xlApp.Visible = true;
+            xlWb.Close(true); //закрыть и сохранить книгу
+            xlApp.Quit();
+            MessageBox.Show("Файл успешно сохранён!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int b = Convert.ToInt32(textBox1.Text);
+            string a = Convert.ToString(dataGridView1.Rows[1].Cells[b].Value);
+            MessageBox.Show(a);
+
+
+        }
+    }
+}
